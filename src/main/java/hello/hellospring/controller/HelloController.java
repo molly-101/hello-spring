591235be87b89e1727d777e4b40ca94d64c29cd0
wotj7687 <wotj7687@naver.com>
2021-03-31 19:13:50 +0900

    learn MVC and template engine

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -13,9 +14,9 @@ public class HelloController {
         return "Jaeseo";
     }
 
-    @GetMapping("test1")
-    public String jaeseo(Model model){
-        model.addAttribute("data", "test1에 접속하신 : 재서 님!");
-        return "Jaeseo";
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
