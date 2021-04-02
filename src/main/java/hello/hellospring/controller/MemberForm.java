59654778c16fadccf8756ba4b94caca4c75f40a8
wotj7687 <wotj7687@naver.com>
2021-04-03 02:09:39 +0900

    make member_web_function

---
+++ src/main/java/hello/hellospring/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package hello.hellospring.controller;
+
+public class MemberForm {
+    private String namu;
+
+    public String getName() {
+        return namu;
+    }
+
+    public void setName(String namu) {
+        this.namu = namu;
+    }
+}
